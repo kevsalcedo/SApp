82a9a6655c72cdcb75501777d1661bfcbc8699c8
KevSalcedo <kevsalcedom@gmail.com>
2023-02-25 20:53:38 -0500

    RecyclerView for Category Cards is done and the splash screen icon was updated to a white flower.

--- app/src/main/java/com/graymandev/sapp/MainActivity.kt
+++ app/src/main/java/com/graymandev/sapp/MainActivity.kt
@@ -65,4 +65,5 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
     }
 
 
+
 }
--- app/src/main/java/com/graymandev/sapp/view/main/SectionListFragment.kt
+++ app/src/main/java/com/graymandev/sapp/view/main/SectionListFragment.kt
@@ -3,6 +3,7 @@ package com.graymandev.sapp.view.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.chetantuteja.easybinding.BindingFragment
 import com.graymandev.sapp.MainActivity
 import com.graymandev.sapp.databinding.FragmentSectionListBinding
@@ -13,6 +14,10 @@ class SectionListFragment : BindingFragment<FragmentSectionListBinding>() {
             val mainActivity = activity as MainActivity
             mainActivity.navigateToStores()
         }
+
+        binding.recyclerViewCategoryCards.layoutManager = GridLayoutManager(activity, 2)
+        binding.recyclerViewCategoryCards.adapter =
+            activity?.let { CategoryCardAdapter(it, CategoryCardProvider.CategoryCardList) }
     }
 
     override fun setupViewBinding(
