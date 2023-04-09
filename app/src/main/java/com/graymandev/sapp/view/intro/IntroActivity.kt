efa4f86733ed322f682f3b5f78a4a961306ce736
KevSalcedo <kevsalcedom@gmail.com>
2023-04-09 13:12:18 -0500

    Introduce fragment to start activity_intro inside the init() function.

--- app/src/main/java/com/graymandev/sapp/view/intro/IntroActivity.kt
+++ app/src/main/java/com/graymandev/sapp/view/intro/IntroActivity.kt
@@ -1,17 +1,27 @@
 package com.graymandev.sapp.view.intro
 
 import android.view.LayoutInflater
+import android.view.Menu
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import com.chetantuteja.easybinding.BindingActivity
+import com.google.firebase.FirebaseApp
+import com.graymandev.sapp.R
 import com.graymandev.sapp.databinding.ActivityIntroBinding
 
 class IntroActivity : BindingActivity<ActivityIntroBinding>() {
 
-    override fun setupViewBinding(inflater: LayoutInflater): ActivityIntroBinding{
-        return ActivityIntroBinding.inflate(inflater)
-    }
+    private lateinit var navController: NavController
+    private lateinit var thisMenu: Menu
 
     override fun init() {
+        val navIntroFragment = supportFragmentManager.findFragmentById(R.id.intro_fragment) as NavHostFragment
+        navController = navIntroFragment.navController
+    }
 
+    override fun setupViewBinding(inflater: LayoutInflater): ActivityIntroBinding{
+        return ActivityIntroBinding.inflate(inflater)
     }
 
 }
